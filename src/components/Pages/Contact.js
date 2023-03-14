08a4130574a3ab34fee0f3648c748b3f9d9e6b2d
IbnHichame <deanbel@berkeley.edu>
2023-03-14 11:15:10 -0700

    implemented email function

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Contact = () => {
+  const [email, setEmail] = useState('');
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle email submission here
+    console.log('Email submitted!', email, subject, message);
+    // Reset form after submission
+    setEmail('');
+    setSubject('');
+    setMessage('');
+  };
+
   return (
     <div>
       <h1>Contact Us</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          Subject:
+          <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} />
+        </label>
+        <label>
+          Message:
+          <textarea value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+        </label>
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 };
+
+//export default Contact;
