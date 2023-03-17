b57f69dbd9d14939c3ea35053f43e8ee62d17184
Misbah Syed <mmsyed03@berkeley.edu>
2023-03-16 17:14:18 -0700

    changes

--- src/components/Pages/Visiting.js
+++ src/components/Pages/Visiting.js
@@ -3,7 +3,15 @@ import React from "react";
 export const Visiting = () => {
   return (
     <div>
-      <h1>Visiting</h1>
+      <h1>Visiting
+
+        <h2> Etiquettes and sunnahs when visiting the grave </h2>
+        <p>
+          When entering the gravesite, say a general salaam and recite this duaa':
+          
+          "Peace be upon the inhabitants of this abode from among the Believers and the Muslims, and may Allah have mercy on those who have gone ahead of us, and those who come later on, and we shall, God willing, join you. (Sahih Muslim)"
+        </p>
+      </h1>
     </div>
   );
 };
