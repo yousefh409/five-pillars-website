0d98466fd7a513628c6c8097c1ec3583ad3ea71f
Zachary Chang <zchang@berkeley.edu>
2023-03-29 18:12:56 -0700

    added some buttons

--- src/components/Pages/Services.js
+++ src/components/Pages/Services.js
@@ -3,8 +3,21 @@ import React from "react";
 export const Services = () => {
   return (
     <div>
-      <h1>Services</h1>
-      <button type="submit" >Submit</button>
+        <h1> Services
+            
+        <h2> Burial Payment
+        <p>
+        <button onclick = "window.location.href='www.paypal.com';">Paypal</button>
+        </p>
+        </h2>
+
+        <h2> Headstone Payment
+        <p>
+        <button onclick = "window.location.href='www.paypal.com';">Paypal</button>
+        </p>
+        </h2>
+
+        </h1>
     </div>
   );
 };
