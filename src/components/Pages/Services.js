29b5e5bd6e04d5499f33b74159b95ab544bd2650
Zachary Chang <zchang@berkeley.edu>
2023-03-31 17:29:39 -0700

    Added some text to services page

--- src/components/Pages/Services.js
+++ src/components/Pages/Services.js
@@ -6,12 +6,18 @@ export const Services = () => {
         <h1> Services
             
         <h2> Burial Payment
+            <p>
+            How to pay burial fees: *Include information here*
+            </p>
         <p>
         <button onclick = "window.location.href='www.paypal.com';">Paypal</button>
         </p>
         </h2>
 
         <h2> Headstone Payment
+            <p>
+            How to pay for a headstone: *Include information here*
+            </p>
         <p>
         <button onclick = "window.location.href='www.paypal.com';">Paypal</button>
         </p>
