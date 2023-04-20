904769d0c04d2b29d75b80284dd4c92be91a9e53
IbnHichame <deanbel@berkeley.edu>
2023-04-19 18:59:02 -0700

    working on dropdown for what to do when someone dies

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,6 +6,12 @@ function NavBar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <>
       <nav className="navbar">
@@ -93,6 +99,54 @@ function NavBar() {
                 Services
               </NavLink>
             </li>
+
+            <li className="nav-item dropdown-toggle" onClick={handleDropdown}>
+                          <NavLink
+                            exact
+                            to="#"
+                            activeClassName="active"
+                            className="nav-links"
+                          >
+                            Dropdown
+                          </NavLink>
+                          {showDropdown && (
+                            <ul className="dropdown-menu">
+                              <li className="dropdown-item">
+                                <NavLink
+                                  exact
+                                  to="/page1"
+                                  activeClassName="active"
+                                  className="nav-links"
+                                  onClick={handleClick}
+                                >
+                                  Page 1
+                                </NavLink>
+                              </li>
+                              <li className="dropdown-item">
+                                <NavLink
+                                  exact
+                                  to="/page2"
+                                  activeClassName="active"
+                                  className="nav-links"
+                                  onClick={handleClick}
+                                >
+                                  Page 2
+                                </NavLink>
+                              </li>
+                            </ul>
+                          )}
+                        </li>
+            <li className="nav-item">
+                          <NavLink
+                            exact
+                            to="/services"
+                            activeClassName="active"
+                            className="nav-links"
+                            onClick={handleClick}
+                          >
+                            Information
+                          </NavLink>
+                        </li>
           </ul>
           <div className="nav-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
