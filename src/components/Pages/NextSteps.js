cb60ae9d89552ee8ef4a0bbc32ab12c205d08aa5
IbnHichame <deanbel@berkeley.edu>
2023-04-30 17:19:02 -0700

    added intro to next steps

--- src/components/Pages/NextSteps.js
+++ src/components/Pages/NextSteps.js
@@ -1,10 +1,38 @@
 import React from "react";
+import "./NextSteps.css";
 
 export const NextSteps = () => {
   return (
+
     <div className="nextStepsContainer">
       <div>
-        <h1>Next Steps</h1>
+
+        <h1><b>What to do When Someone Dies</b></h1>
+        <h2> Introduction
+                <p><br></br>
+                    The death of someone in the family or of a friend is often a time of great stress and emotion.
+
+                    Quite often people are not familiar with what to do, either from a religious point of view or in terms of the practical steps that need to be completed to bury one’s loved ones.
+
+                    This booklet is intended to address the above needs.
+
+                    The practical steps that are covered relate to the paperwork which needs to be completed to meet legal requirements; and to organising the funeral itself.
+
+                    What is most important to realise is that if you are unsure about any aspect, or if you need help and support, members of the extended family, close friends and your local Mosque are always available to help and consult.
+
+                    It is Sunnah, on hearing of the death of a fellow Muslim, to recite the following brief prayer: <br></br><br></br>
+
+
+                </p>
+                <p class="arabicDua"><b>إِنَّا لِلّهِ وَإِنَّـا إِلَيْهِ رَاجِعُونَ</b></p>
+
+              <br></br>
+                <p> Translation: <br></br>
+                    Indeed to Allãh do we belong and to Him we shall return.
+                     </p> <p class="translation">(Sũrah alBaqarah Verse 156, Sahĩh Muslim 918)</p>
+                </h2>
+
+
       </div>
     </div>
   );
---
+++ src/components/Pages/WhenSomeoneDies.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const WhenSomeoneDies = () => {
+  return (
+    <div>
+      <h1>Visiting
+
+        <h2> Etiquettes and sunnahs when visiting the grave
+        <p>
+            1. When entering the gravesite, say a general salaam to the believers and recite this duaa': <br></br>
+             "Peace be upon the inhabitants of this abode from among the Believers and the Muslims, and may Allah have mercy on those who have gone ahead of us, and those who come later on, and we shall, God willing, join you. (Sahih Muslim)" <br></br>
+            2. Recite Qur’an with the intention of the reward going to the deceased. <br></br>
+            3. Make abundant supplication for their forgiveness. <br></br>
+            4. Reflect and ponder on the state of the deceased and on one’s own state. Visiting the grave is a solemn reminder of death, and reflection of such a weighty and inevitable state is important. <br></br>
+        </p>
+        </h2>
+
+        <h2>Forbidden Practices
+        <p>As with all things, there are boundaries that we must stay within, as defined by the Shari'ah. Visiting the grave is an action that requires composure and solemnity. One should avoid excessive crying and wailing, which the Prophet ﷺ forbade.
+
+
+        </p>
+        </h2>
+
+      </h1>
+    </div>
+  );
+};
+
