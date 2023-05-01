48f95160491044ad2475a8fda7964e614c07bc1f
Selin Musa <smusa@berkeley.edu>
2023-04-30 17:19:59 -0700

    style

--- src/components/Pages/Services.js
+++ src/components/Pages/Services.js
@@ -25,7 +25,7 @@ export const Services = () => {
         Burial Form
         </button>
 
-      <h2 class = "offer1">Prepaid Gravesite:<b></b>
+      <h2 class = "offer1">Prepaid Gravesite:<b></b> </h2>
       <p>For those who already have a gravesite paid for.</p><b></b>
       <p>Services include: </p>
       <p>
@@ -36,25 +36,21 @@ export const Services = () => {
       <ul>E. Endowment Fee</ul>
       Total: $3900.00
       </p><b></b>
-      </h2>
-      <h2 class = "offer2">
-      Gravesite and Services:<b></b>
+      <h2> Gravesite and Services:</h2>
       <p>For those who do not yet have a gravesite, an extra $2000 is needed to purchase one. This package includes all of the preceding services. The total will be $5900.</p>
-      </h2>
+      
 
-      <h2 >
-        Children<b></b>
+      <h2 >Children</h2>
         <p>For children up to 34" in length, the burial fees are $2500.</p>
-      </h2>
+      
       <h2>
-        Extra Fees: 
+        Extra Fees: </h2>
         <p>
-          <b></b>
          If the burial is on a weekend or Holiday, an extra $500 will be charged. <br></br><br></br>
          If the burial goes beyond the scheduled time, and extra $150 will be charged per hour. <br></br>
       
         </p>
-      </h2>
+      
     </div> 
   );
 }
