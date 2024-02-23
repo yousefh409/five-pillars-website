ee1d090b3638b4d56b026b42d57c3ef9525746e5
yousefh409 <yousefh409@gmail.com>
2024-02-22 20:33:37 -0800

    fixerd

--- src/Pages/Acknowledgement/Acknowledgement.js
+++ src/Pages/Acknowledgement/Acknowledgement.js
@@ -12,7 +12,9 @@ export const Acknowledgement = () => {
         <p className="mb-2 ml-6">
         We would like to extend heartfelt appreciation to the UC Berkeley Muslim Tech Collaborative (MTC) for their invaluable contribution in developing the website for the Five Pillars Cemetery. Their dedication and expertise have played a pivotal role in creating a platform that not only provides essential information about the cemetery but also includes a finder tool to locate buried loved ones. Through their innovative approach and commitment to serving the community, the MTC has empowered individuals to navigate the delicate process of locating and honoring their departed family members and friends with ease and efficiency. Their efforts underscore the profound impact that technology can have in enhancing accessibility and providing solace during difficult times.<br></br>
           <br></br>
-        We would like to thank the members of MTC who worked on this project, specifically: Selin Musa, Yousef Helal, Abdullah Mansour.
+        We would like to thank the members of MTC who worked on this project, specifically: Selin Musa, Yousef Helal, Abdullah Mansour, Nasreddine Belhoucine, Riyya Ahmed, and Misbah Syed.
+        <br></br>
+        We would also like to thank members of the comunity who contributed extensivley to making this project a reality: Shazia Kajani, Basil Rizwan, and Imam Tahir.
         <br></br>
         </p>
       </div>
