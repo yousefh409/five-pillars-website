3a3a18c25040b8db9a9db69926f31bf0b0b18cea
yousefh409 <yousefh409@gmail.com>
2024-02-22 21:36:18 -0800

    phone fix

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,18 +1,53 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { NavLink } from 'react-router-dom';
-import './NavBar.css';
+import './NavBar.scss';
 import logo from '../../images/logo.png';
+import { Link, useNavigate } from "react-router-dom";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
 
 function NavBar() {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
 
+  const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: 0,
+    height: 0,
+  });
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 768 && menuOpen) {
+      setMenuOpen(false);
+    }
+  }, [size.width, menuOpen]);
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  }
+
   return (
     <div className="bg-green-900 sticky top-0 z-50">
       <div className="flex center">
         <img className="navbarLogo" alt="" src={logo}></img>
-        <ul className="text-center pt-6 lg:pt-12 pb-8 lg:pb-3 px-8 lg:px-16 title-font tracking-wide text-xl sm:text-2xl md: text-3xl lg:text-5xl text-white">
+        <ul className="text-center pt-6 lg:pt-12  lg:px-16 title-font tracking-wide text-xl sm:text-2xl md: text-3xl lg:text-3xl text-white">
           <a
             href="/"
             style={{
@@ -27,16 +62,20 @@ function NavBar() {
         <img className="navbarLogo" alt="" src={logo}></img>
       </div>
 
-      <div className="">
-        <nav>
-          <div className="flex justify-center items-center text-font text-white ">
+      <header className="header">
+      <div className="header__content">
+      <nav
+          className={`${"header__content__nav"} 
+          ${menuOpen && size.width < 768 ? `${"isMenu"}` : ""} 
+          }`}
+        >          <div className="flex justify-center items-center text-font text-white ">
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
               <li className="nav-item">
                 <NavLink
                   exact
                   to="/"
                   activeClassName="active"
-                  onClick={handleClick}
+                  onClick={() => {closeMenu(); handleClick();}}
                 >
                   Home
                 </NavLink>
@@ -46,7 +85,7 @@ function NavBar() {
                   exact
                   to="/next-steps"
                   activeClassName="active"
-                  onClick={handleClick}
+                  onClick={() => {closeMenu(); handleClick();}}
                 >
                   Next Steps
                 </NavLink>
@@ -56,7 +95,7 @@ function NavBar() {
                   exact
                   to="/janaza"
                   activeClassName="active"
-                  onClick={handleClick}
+                  onClick={() => {closeMenu(); handleClick();}}
                 >
                   Janaza
                 </NavLink>
@@ -66,7 +105,7 @@ function NavBar() {
                   exact
                   to="/services"
                   activeClassName="active"
-                  onClick={handleClick}
+                  onClick={() => {closeMenu(); handleClick();}}
                 >
                   Services
                 </NavLink>
@@ -76,7 +115,7 @@ function NavBar() {
                   exact
                   to="/forms"
                   activeClassName="active"
-                  onClick={handleClick}
+                  onClick={() => {closeMenu(); handleClick();}}
                 >
                   Forms
                 </NavLink>
@@ -86,7 +125,7 @@ function NavBar() {
                   exact
                   to="/visiting"
                   activeClassName="active"
-                  onClick={handleClick}
+                  onClick={() => {closeMenu(); handleClick();}}
                 >
                   Visiting
                 </NavLink>
@@ -96,7 +135,7 @@ function NavBar() {
                   exact
                   to="/map"
                   activeClassName="active"
-                  onClick={handleClick}
+                  onClick={() => {closeMenu(); handleClick();}}
                 >
                   Map
                 </NavLink>
@@ -106,7 +145,7 @@ function NavBar() {
                   exact
                   to="/donations"
                   activeClassName="active"
-                  onClick={handleClick}
+                  onClick={() => {closeMenu(); handleClick();}}
                 >
                   Donating
                 </NavLink>
@@ -116,7 +155,7 @@ function NavBar() {
                   exact
                   to="/contact"
                   activeClassName="active"
-                  onClick={handleClick}
+                  onClick={() => {closeMenu(); handleClick();}}
                 >
                   Contact Us
                 </NavLink>
@@ -126,7 +165,7 @@ function NavBar() {
                   exact
                   to="/acknowledgement"
                   activeClassName="active"
-                  onClick={handleClick}
+                  onClick={() => {closeMenu(); handleClick();}}
                 >
                   Acknowledgements
                 </NavLink>
@@ -192,7 +231,15 @@ function NavBar() {
             </div>
           </div>
         </nav>
+        <div className="header__content__toggle">
+          {!menuOpen ? (
+            <BiMenuAltRight onClick={menuToggleHandler} />
+          ) : (
+            <AiOutlineClose onClick={menuToggleHandler} />
+          )}
+        </div>
       </div>
+      </header>
     </div>
   );
 }
