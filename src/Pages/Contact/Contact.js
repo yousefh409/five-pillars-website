3b60252e8ede13d8c5074a0ae1109b706dac846b
basilrizwan <basil@goldenpen.io>
2024-06-05 18:14:38 -0700

    Update Contact.js

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -26,24 +26,12 @@ export const Contact = () => {
             <div className="ml-6">
               {/* Contact Info */}
               <div className="ml-6">
-                <div className="subtitle-font text-3xl text-black py-4">
-                  Phone
-                </div>
-                <p className="text-lg">(510) 552-7755 - Adnan Zaki</p>
-                <p className="text-lg">(510) 517-8397 - Mohammad Zaki</p>
-                <div className="subtitle-font text-3xl text-black py-4">
+            
                   Email
                 </div>
-                <p className="text-lg">info@5pillarscemetery.com</p>
+                <p className="text-lg">info@janazas.org</p>
                 <div className="subtitle-font text-3xl text-black py-4">
-                  Hours
-                </div>
-                <p className="text-lg">Monday through Sunday, 10AM - 5PM</p>
-                <div className="subtitle-font text-3xl text-black py-4">
-                  Address
-                </div>
-                <p className="text-lg">1761 Laughlin Rd,</p>
-                <p className="text-lg">Livermore, CA 94551</p>
+                
               </div>
             </div>
           </div>
